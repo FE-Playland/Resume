c96658bae0f59d1ffc8ede2ca06a5badf01a7ca7
X140Yu <X140Yu@users.noreply.github.com>
2021-06-24 07:12:59 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.298ff56eb2e7e880ab10ed7a791c7094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125f67679b6064042a0354aa92f2d4d3",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "8c49c19bf8f8443b0c17",
+    "url": "/Resume/static/js/2.a79fd63f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/Resume/static/js/2.a79fd63f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f782a5123e63927f261d",
+    "url": "/Resume/static/js/main.451d5cac.chunk.js"
+  },
+  {
+    "revision": "04191604dc1b4b2ada16",
+    "url": "/Resume/static/js/runtime-main.fda79c29.js"
+  }
+]);
